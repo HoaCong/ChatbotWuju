550239fb6f2ed1cfa4cc88608b55e872221b0042
dev-hoale <leconghoa11a1@gmail.com>
2023-05-31 17:56:05 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,5 +1,5 @@
 // require("dotenv").config();
-
+import request from "request";
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
@@ -89,7 +89,7 @@ function callSendAPI(sender_psid, response) {
   request(
     {
       uri: "https://graph.facebook.com/v2.6/me/messages",
-      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      qs: { access_token: PAGE_ACCESS_TOKEN },
       method: "POST",
       json: request_body,
     },
