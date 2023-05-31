072bbdef6ff0d58617b4bcb01550e3b4b17a2b2a
dev-hoale <leconghoa11a1@gmail.com>
2023-05-31 23:55:08 +0700

    update

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.setHeader("Cache-Control", "no-store");
   next();
 });
 
