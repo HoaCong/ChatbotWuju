889969af719cbea6e724090f95019e30ff6f55d7
dev-hoale <leconghoa11a1@gmail.com>
2023-05-31 21:32:05 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -4,7 +4,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
 let getHomePage = (req, res) => {
-  return res.send("Đã thay đổi");
+  res.json({ message: "Hello, World!" });
 };
 
 let getWebhook = (req, res) => {
