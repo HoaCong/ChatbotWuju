3a50eec371cc7a337824d187deb2b37e82a8bcbf
dev-hoale <leconghoa11a1@gmail.com>
2023-05-31 23:47:02 +0700

    update

--- src/server.js
+++ src/server.js
@@ -6,6 +6,15 @@ require("dotenv").config();
 
 let app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // config viewEngine
 viewEngine(app);
 
