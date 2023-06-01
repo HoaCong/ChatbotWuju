43e579c4be5c5d841d414859d1e093e2c0a94dc0
dev-hoale <leconghoa11a1@gmail.com>
2023-06-01 15:20:54 +0700

    udater

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -36,6 +36,7 @@ let postWebhook = (req, res) => {
     body.entry.forEach((entry) => {
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
+      console.log("body.entry.forEach  webhook_event:", webhook_event);
 
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
